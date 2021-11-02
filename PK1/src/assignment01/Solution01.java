be963725410907b4a452844563c8ba6e16743158
Kai Leibold <kai.leibold@uni-konstanz.de>
2021-11-02 22:35:11 +0100

    Added Solution 2

--- PK1/src/assignment01/Solution01.java
+++ PK1/src/assignment01/Solution01.java
@@ -3,7 +3,7 @@ package assignment01;
 public class Solution01 {
 
     public static void main(String[] args) {
-        
+
         System.out.println("  /\\");
         System.out.println(" /°_\\");
         System.out.println("/__°_\\");
---
+++ PK1/src/assignment01/Solution02.java
@@ -0,0 +1,19 @@
+package assignment01;
+
+public class Solution02 {
+
+    public static void main(String[] args) {
+
+        short studentsAttendingPK1 = 182;
+        short studentsEnroled = 12000;
+        int populationEU = 447706209;
+        long populationEarth = 7753000000l;
+        byte semestersPlanned = 6;
+        char lastName = 'L';
+        float averageGrade = 1.0f;
+        boolean isLeapYear = false;
+
+
+    }
+
+}

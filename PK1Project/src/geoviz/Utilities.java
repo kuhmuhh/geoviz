99e2995976ab35228834d2db97e3593eeff1a99f
Kai Leibold <kai.leibold@uni-konstanz.de>
2021-11-04 18:42:21 +0100

    implemented round2 method

--- PK1Project/src/geoviz/Utilities.java
+++ PK1Project/src/geoviz/Utilities.java
@@ -5,6 +5,7 @@ public class Utilities {
     public static void main(String[] args) {
 
         System.out.println("yo");
+        System.out.println(round2(2.046234183));
 
 
     }
@@ -16,5 +17,26 @@ public class Utilities {
 
     }
     */
+
+    public static double round2(double value){
+
+        //double number = 2.046234183;
+        double firstStep;
+        int secondStep;
+
+        firstStep = value * 100;
+
+        //System.out.println(firstStep);
+
+        secondStep = (int)firstStep;
+
+        value = secondStep;
+        value = value / 100;
+
+        //System.out.println(secondStep);
+        //System.out.println(value);
+        return value;
+
+    }
 
 }

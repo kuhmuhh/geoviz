232c7b87ac31a827f27e588754bd53990e6a6233
Kai Leibold <kai.leibold@uni-konstanz.de>
2021-12-01 19:33:59 +0100

    started assignment06

---
+++ PK1/src/assingment06/MyString.java
@@ -0,0 +1,19 @@
+package assingment06;
+
+public class MyString {
+
+    private char[] string;
+
+    public MyString(){}
+
+    public MyString(char... values){
+        this.string = values;
+    }
+
+
+    public MyString(char[] array){
+        this.string = array;
+    }
+
+
+}
---
+++ PK1/src/assingment06/Solution06.java
@@ -0,0 +1,10 @@
+package assingment06;
+
+public class Solution06 {
+
+    public static void main(String[] args) {
+
+        //MyString name = new MyString(new char[]{'S', 'a'});
+    }
+
+}

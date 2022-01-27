beb48c3804a9b73c5ec467e87d77b6eff6259770
kai.leibold <kai.leibold@uni-konstanz.de>
2022-01-27 11:29:50 +0100

    assignment12

--- PK1/src/assignment12/Window.java
+++ PK1/src/assignment12/Window.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 
 public class Window extends Application {
 
-    private static ArrayList<MyPoint> list;
+    private static ArrayList<MyPoint> list = new ArrayList<MyPoint>();
     private static Pane pane;
     private static Label showIt;
 
@@ -24,8 +25,6 @@ public class Window extends Application {
     public void start(Stage primaryStage){
 
         Label showIt = new Label();
-        ArrayList<MyPoint> list = new ArrayList<>();
-        this.list = list;
         Pane root = new Pane();
         this.pane = root;
         Scene scene = new Scene(root, 500, 400);
@@ -40,7 +39,38 @@ public class Window extends Application {
         Circle circle = new Circle(point.getX(), point.getY(), 5);
         pane.getChildren().add(circle);
         list.add(point);
-        return;
+        System.out.println(line());
     }
+
+    public static double line(){
+        int size = list.size();
+        double xAverage = 0;
+        double yAverage = 0;
+        double b = 0;
+        double a = 0;
+
+        for (int i = 0; i < size ; i++) {
+            xAverage = list.get(i).getX() + xAverage;
+            yAverage = list.get(i).getY() + yAverage;
+        }
+
+        xAverage = xAverage / size;
+        yAverage = yAverage / size;
+
+        for (int i = 0; i < size; i++){
+            b = ((list.get(i).getX() - xAverage)*(list.get(i).getY() - yAverage))/((list.get(i).getX() - xAverage)*(list.get(i).getX() - xAverage));
+        }
+
+        a = yAverage - b * xAverage;
+
+        return a;
+    }
+
+    public static void addLine(MyPoint point1, MyPoint point2){
+        Line line = new Line(point1.getX(), point1.getY(), point2.getX(), point2.getY());
+        pane.getChildren().add(line);
+    }
+
+
 
 }
